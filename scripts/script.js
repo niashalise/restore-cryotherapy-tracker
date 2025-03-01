e088db0ce81f9b7b4a90885ef2a16a3cdc53785a
Nia <shalise.nia@gmail.com>
2025-03-01 12:54:28 -0500

    Created and styled each HTML page, adding responsiveness and necessary page elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,29 @@
-let hamburg = document.getElementById("hamburg");
-let nav = document.getElementById("nav")
+// let hamburg = document.getElementById("ham-menu");
+// let offScreenMenu = document.getElementById("off-screen-menu");
 
-hamburg.addEventListener("onclick", () => {
-    if (nav.style.display === "none") {
-        nav.style.display = "block";
-    } else {
-        nav.style.display = "none";
-    }
+
+// hamburg.addEventListener("click", () => {
+//     hamburg.classList.toggle('active');
+// })
+
+// Index Buttons
+const startSession = document.querySelector('.start');
+const todaysSessions = document.querySelector('.sessions');
+const search = document.querySelector('.search');
+const jump = document.querySelector('.jump');
+
+startSession.addEventListener('click', () => {
+    window.open("./newsession.html");
+})
+
+todaysSessions.addEventListener('click', () => {
+    window.open("./currentsessions.html");
+})
+
+search.addEventListener('click', () => {
+    // this will trigger a pop up search to find a specific client
+})
+
+jump.addEventListener('click', () => {
+    // this will trigger a pop up calendar to jump to a date from the past
 })
